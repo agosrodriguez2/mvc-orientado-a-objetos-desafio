dabbc5087aa7e63c028a6b8fa5b6ed555475e1cd
Agostina Rodriguez <agosrodriguezdev@gmail.com>
2025-12-08 22:09:53 -0300

    se agrego la implementacion del controllers y models

--- source/models.ts
+++ source/models.ts
@@ -1,5 +1,7 @@
 // este import existe solo para que tsc lo tome y lo copie a /build
-import "./contacts.json";
+import * as jsonData from "./contacts.json";
+import * as fs from "fs";
+import * as path from "path";
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
 
@@ -8,5 +10,34 @@ class Contact {
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  private data: any[] = [];
+  private filePath = path.resolve(__dirname, "contacts.json");
+
+  load() {
+    const file = fs.readFileSync(this.filePath).toString();
+    this.data = JSON.parse(file);
+  }
+
+  getAll() {
+    return this.data;
+  }
+
+  addOne(contact: Contact) {
+    if (contact == null) {
+      throw new Error("Contacto vacio");
+    }
+
+    this.data.push(contact);
+  }
+
+  save() {
+    fs.writeFileSync(this.filePath, JSON.stringify(this.data, null, 2));
+  }
+
+  getOneById(id) {
+    return this.data.find((contacto) => contacto.id == id);
+  }
+}
+
 export { ContactsCollection };
